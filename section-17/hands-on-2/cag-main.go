cd13d223fe6f8b733e0927560e50ce2729447287
Carlos A. Gorricho <cgorricho@heptagongroup.co>
2021-02-19 19:53:41 -0500

    trabajo CAG 19-02-2021

--- section-17/hands-on-2/cag-main.go
+++ section-17/hands-on-2/cag-main.go
@@ -12,11 +12,24 @@ func main() {
 	}
 
 	fmt.Println(p1)
-	cambio(&p1) // se pasa la drieccion a la variable
+	cambio1(&p1) // se pasa la drieccion a la variable
 	fmt.Println(p1)
+
+	p2 := person{
+		nombre: "Maria Gorricho",
+	}
+
+	fmt.Println(p2)
+	p2 = cambio2(p2) // se pasa la variable
+	fmt.Println(p2)
 
 }
 
-func cambio(p *person) {
+func cambio1(p *person) {
 	p.nombre = "Carlos Alejandro"
+}
+
+func cambio2(p person) person {
+	p.nombre = "Maria Andrea"
+	return p
 }
