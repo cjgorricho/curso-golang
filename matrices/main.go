bdccc460d9f293c8d116462c458611b344361498
Carlos A. Gorricho <cgorricho@heptagongroup.co>
2021-02-25 22:00:20 -0500

    trabajo CAG 25-02-2021

--- matrices/main.go
+++ matrices/main.go
@@ -14,8 +14,8 @@ func randmat(mat [][]int, rows, cols int) [][]int {
 		var row []int
 		for j := 0; j < cols; j++ {
 			rand.Seed(time.Now().UnixNano())
-			time.Sleep(time.Millisecond)
-			value := rand.Intn(cols)
+			time.Sleep(5 * time.Millisecond)
+			value := rand.Intn(5) + 1
 			row = append(row, value)
 		}
 		mat = append(mat, row)
@@ -42,36 +42,36 @@ func mult(mat, mat1, mat2 [][]int) [][]int {
 			pos := 0
 			for j, mt2 := range mat2 {
 				pos = pos + mt1[j]*mt2[k]
+				//fmt.Println(i, j, pos)
 			}
 			row = append(row, pos)
 		}
 		mat = append(mat, row)
 	}
+	//fmt.Println(mat)
 	return mat
+}
+
+func printmat(m ...[][]int) {
+	for m, mat := range m {
+		fmt.Printf("Matriz %v\nTipo: %T\nTamaño :%v\nCapacidad :%v\n\n", m+1, mat, len(mat), cap(mat))
+
+		if len(mat) <= 10 && len(mat[0]) <= 10 {
+			fmt.Printf("Matriz\n")
+			for i, mt := range mat {
+				fmt.Printf("%d\t%v\n", i, mt)
+			}
+		}
+	}
 }
 
 func main() {
 	start := time.Now()
 	var m1, m2 [][]int
 	var m3 [][]int
-	m1 = randmat(m1, 5, 10)
-	m2 = randmat(m2, 10, 20)
+	m1 = randmat(m1, 5, 300)
+	m2 = randmat(m2, 300, 10)
 	m3 = mult(m3, m1, m2)
-	fmt.Printf("\n%.3fms elapsed\n\n", float64(time.Since(start).Microseconds())/1000)
-	fmt.Printf("Matriz 1\nTipo: %[1]T\nTamaño :%v\nCapacidad :%v\n", m1, len(m1), cap(m1))
-	fmt.Printf("\nMatriz\n")
-	for i, mat := range m1 {
-		fmt.Printf("%d\t%v\n", i, mat)
-	}
-	fmt.Printf("\nMatriz 2\nTipo: %[1]T\nTamaño :%v\nCapacidad :%v\n", m2, len(m2), cap(m2))
-	fmt.Printf("\nMatriz\n")
-	for i, mat := range m2 {
-		fmt.Printf("%d\t%v\n", i, mat)
-	}
-	fmt.Printf("\nMatriz 3\nTipo: %[1]T\nTamaño :%v\nCapacidad :%v\n", m3, len(m3), cap(m3))
-	fmt.Printf("\nMatriz\n")
-	for i, mat := range m3 {
-		fmt.Printf("%d\t%v\n", i, mat)
-	}
-
+	fmt.Printf("\n%.3f seconds elapsed\n\n", time.Since(start).Seconds())
+	printmat(m1, m2, m3)
 }
